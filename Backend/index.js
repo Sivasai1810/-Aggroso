0959599dfb04892626790f4d886c5db5e8a41f64
Sivasai1810 <nsivasai88@gmail.com>
2026-02-16 20:31:13 +0530

    allowed the cors for deployed url

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import router from "./Router/workflowrouter.js"
 const app=express()
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://aggroso-blue.vercel.app/",
   credentials:true
 }))
 app.use(express.json())
