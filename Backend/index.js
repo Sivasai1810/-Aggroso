32b4b610fa6647d3a0df6e6ad215323f4a78a3d1
Sivasai1810 <nsivasai88@gmail.com>
2026-02-16 20:52:35 +0530

    ui

--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,25 @@ import express from "express"
 import cors from "cors"
 import router from "./Router/workflowrouter.js"
 const app=express()
-app.use(cors({
-  origin:"https://aggroso-blue.vercel.app/",
-  credentials:true
-}))
+const allowedOrigins = [
+  "http://localhost:5173",    
+  "https://aggroso-blue.vercel.app"
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json())
 app.use("/sent",router);
 
